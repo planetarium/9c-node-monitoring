d0187c8def43a98aff7ec4df177ba2253493f58f
Jeongho <hohoho4u4me@gmail.com>
2024-12-30 10:06:58 +0900

    Initialize main branch for refactored project

--- src/api/db/node-health/node-health.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { CustomRepository } from "../typeorm-ex.decorator";
-import { NodeHealth } from "./node-health";
-
-
-@CustomRepository(NodeHealth)
-export class NodeHealthRepository extends Repository<NodeHealth> {}
