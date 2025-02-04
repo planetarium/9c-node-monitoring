004bd862160037e19fea059d49b0dd72ccf8c3c0
Jeongho <hohoho4u4me@gmail.com>
2025-02-04 17:20:25 +0900

    Update CORS policy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', //TODO: 배포 완료 후 cors 변경
+    origin: ['http://3.35.227.125:3000'], // 웹 서버 주소만 허용
     credentials: false,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
